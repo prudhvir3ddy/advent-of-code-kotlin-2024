9795e1dd2c413d83ede705f9ace90526d2bb4fd0
prudhvir3ddy <>
2024-12-03 13:01:01 +0530

    day 3 part 2 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,11 +1,14 @@
 fun main() {
 
-    fun matchesPrefix(data: String, i: Int): Boolean {
-        return i + 3 <= data.lastIndex &&
-            data[i] == 'm' &&
-            data[i + 1] == 'u' &&
-            data[i + 2] == 'l' &&
-            data[i + 3] == '('
+    fun matchesPrefix(data: String, i: Int, prefix: String): Boolean {
+        var i  = i
+        var j = 0
+        while (i <= data.lastIndex && j <= prefix.lastIndex && data[i] == prefix[j]) {
+            i++
+            j++
+        }
+
+        return j == prefix.length
     }
 
     fun getSuffixResult(data: String, i: Int): Pair<Boolean, Int> {
@@ -43,7 +46,7 @@ fun main() {
         input.forEach { data ->
             var i = 0
             while (i <= data.lastIndex) {
-                if (matchesPrefix(data, i)) {
+                if (matchesPrefix(data, i, "mul(")) {
                     val suffixResult = getSuffixResult(data, i + 4)
                     if (suffixResult.first) {
                         result += suffixResult.second.toLong()
@@ -55,12 +58,35 @@ fun main() {
         return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun checkFlagInData(flag: Boolean, data: String, i: Int): Boolean {
+        if (matchesPrefix(data, i, "don't()"))
+            return false
+        if (matchesPrefix(data, i, "do()"))
+            return true
+        return flag
+    }
+
+    fun part2(input: List<String>): Long {
+        var result = 0L
+        var shouldDo = true
+        input.forEach { data->
+            var i = 0
+            while (i <= data.lastIndex) {
+                shouldDo = checkFlagInData(shouldDo, data, i)
+                if (shouldDo && matchesPrefix(data, i, "mul(")) {
+                    val suffixResult = getSuffixResult(data, i + 4)
+                    if (suffixResult.first) {
+                        result += suffixResult.second.toLong()
+                    }
+                }
+                i++
+            }
+        }
+        return result
     }
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
-    part1(input).println()
-//    part2(input).println()
+    part1(input).println() // 173731097
+    part2(input).println()
 }
